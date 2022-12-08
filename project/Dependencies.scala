f8ba15c81b06c6d44d20f7a68e437e6e1f281e4b
Scala Steward <scala_steward@virtuslab.com>
2022-12-08 18:34:27 +0000

    Update enumeratum to 1.7.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   private object Versions {
-    val Enumeratum      = "1.7.0"
+    val Enumeratum      = "1.7.2"
     val OrganizeImports = "0.6.0"
     val ScalaTest       = "3.2.14"
   }
