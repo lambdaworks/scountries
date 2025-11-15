701a5c02a9421c9c8b8b02fde733d2fa81a9c5f8
Devin Rathod <119806596+devind-ra@users.noreply.github.com>
2025-11-15 22:13:17 +0000

    refactor: Use ScalaTest Matchers for better test readability (#197)

--- src/test/scala/io/lambdaworks/scountries/ApiSpec.scala
+++ src/test/scala/io/lambdaworks/scountries/ApiSpec.scala
@@ -1,54 +1,67 @@
 package io.lambdaworks.scountries
 
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec._
 
-final class ApiSpec extends AnyWordSpec {
+final class ApiSpec extends AnyWordSpec with Matchers {
 
   "getByCountryName" should {
     "return Some of Country" when {
       "a valid country name with proper case is specified" in {
-        assert(Country.getByCountryName("Serbia").contains(Country.RS))
+        Country.getByCountryName("Serbia") shouldBe Some(Country.RS)
       }
 
       "a valid country name with a different case is specified" in {
-        assert(Country.getByCountryName("sErBiA").contains(Country.RS))
+        Country.getByCountryName("sErBiA") shouldBe Some(Country.RS)
       }
     }
 
     "return None when an invalid country name is specified" in {
-      assert(Country.getByCountryName("Europe").isEmpty)
+      Country.getByCountryName("Europe") shouldBe None
+    }
+
+    "return None when an empty string is specified" in {
+      Country.getByCountryName("") shouldBe None
     }
   }
 
   "getByAlpha2" should {
     "return Some of Country" when {
       "a valid alpha2 with upper case is specified" in {
-        assert(Country.getByAlpha2("RS").contains(Country.RS))
+        Country.getByAlpha2("RS") shouldBe Some(Country.RS)
       }
 
       "a valid alpha2 with a different case is specified" in {
-        assert(Country.getByAlpha2("rS").contains(Country.RS))
+        Country.getByAlpha2("rS") shouldBe Some(Country.RS)
       }
     }
 
     "return None when an invalid alpha2 is specified" in {
-      assert(Country.getByCountryName("EU").isEmpty)
+      Country.getByAlpha2("EU") shouldBe None
+    }
+
+    "return None when an empty string is specified" in {
+      Country.getByAlpha2("") shouldBe None
     }
   }
 
   "getByAlpha3" should {
     "return Some of Country" when {
       "a valid alpha3 with upper case is specified" in {
-        assert(Country.getByAlpha3("SRB").contains(Country.RS))
+        Country.getByAlpha3("SRB") shouldBe Some(Country.RS)
       }
 
       "a valid alpha3 with a different case is specified" in {
-        assert(Country.getByAlpha3("sRb").contains(Country.RS))
+        Country.getByAlpha3("sRb") shouldBe Some(Country.RS)
       }
     }
 
     "return None when an invalid alpha3 is specified" in {
-      assert(Country.getByCountryName("EUR").isEmpty)
+      Country.getByAlpha3("EUR") shouldBe None
+    }
+
+    "return None when an empty string is specified" in {
+      Country.getByAlpha3("") shouldBe None
     }
   }
 
