8b777a30efb0777ea4f1f62eb92f67ea5a455496
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-11-24 16:59:08 +0100

    Update enumeratum to 1.9.1 (#201)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   private object Versions {
-    val Enumeratum      = "1.9.0"
+    val Enumeratum      = "1.9.1"
     val OrganizeImports = "0.6.0"
     val ScalaTest       = "3.2.19"
   }
