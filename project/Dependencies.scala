63f114ab3b67b99024c955ec7e753bdae4203ea5
Isidora Pavlovic <isidorapavlovic0@gmail.com>
2026-02-26 09:58:01 +0100

    feat: add ScalaCheck property-based testing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,11 +5,15 @@ object Dependencies {
   private object Versions {
     val Enumeratum      = "1.9.5"
     val OrganizeImports = "0.6.0"
+    val ScalaCheck      = "1.18.0"
     val ScalaTest       = "3.2.19"
+    val ScalaTestPlus   = "3.2.19.0"
   }
 
-  val enumeratum: ModuleID      = "com.beachape"         %% "enumeratum"       % Versions.Enumeratum
-  val organizeImports: ModuleID = "com.github.liancheng" %% "organize-imports" % Versions.OrganizeImports
-  val scalaTest: ModuleID       = "org.scalatest"        %% "scalatest"        % Versions.ScalaTest % Test
+  val enumeratum: ModuleID              = "com.beachape"         %% "enumeratum"       % Versions.Enumeratum
+  val organizeImports: ModuleID         = "com.github.liancheng" %% "organize-imports" % Versions.OrganizeImports
+  val scalaCheck: ModuleID              = "org.scalacheck"       %% "scalacheck"       % Versions.ScalaCheck    % Test
+  val scalaTest: ModuleID               = "org.scalatest"        %% "scalatest"        % Versions.ScalaTest     % Test
+  val scalaTestPlusScalacheck: ModuleID = "org.scalatestplus"    %% "scalacheck-1-18"  % Versions.ScalaTestPlus % Test
 
 }
